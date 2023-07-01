f19393f9344c43f68594eecb16263f4b82ea6775
danaikrit <danaikrit_j@cmu.ac.th>
2023-07-02 01:16:25 +0700

    sale or not

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ createApp({
         const description = ref('Comfortable socks made with high-quality materials.')
         const image = ref('./assets/images/socks_green.jpg')
         const link = ref('https://www.camt.cmu.ac.th')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
-            product,description,image,link
+            product,description,image,link,inStock,inventory,
+            onSale
         }
     }
 }).mount('#app')
