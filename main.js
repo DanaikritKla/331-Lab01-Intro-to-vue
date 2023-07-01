0df8abbf48dd4f7903f991b85e3074ac2dca99e0
danaikrit <danaikrit_j@cmu.ac.th>
2023-07-02 01:00:44 +0700

    add link product name

--- main.js
+++ main.js
@@ -5,9 +5,10 @@ createApp({
     setup(){
         const product = ref('Boots')
         const description = ref('Comfortable socks made with high-quality materials.')
-         
+        const image = ref('./assets/images/socks_green.jpg')
+        const link = ref('https://www.camt.cmu.ac.th')
         return {
-            product,description
+            product,description,image,link
         }
     }
 }).mount('#app')
